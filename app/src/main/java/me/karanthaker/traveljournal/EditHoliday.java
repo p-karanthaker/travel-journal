954d2b9ece7663125e260888e76bd49de1028a35
Karan Thaker <karan.thaker@hotmail.co.uk>
2018-03-04 16:02:26 +0000

    Title for edit holiday being set

--- app/src/main/java/me/karanthaker/traveljournal/EditHoliday.java
+++ app/src/main/java/me/karanthaker/traveljournal/EditHoliday.java
@@ -2,6 +2,7 @@ package me.karanthaker.traveljournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EditHoliday extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class EditHoliday extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_holiday);
+
+        final TextView activityTitle = (TextView) findViewById(R.id.holidayActivityTitle);
+        activityTitle.setText(R.string.edit_holiday);
+
     }
 }
